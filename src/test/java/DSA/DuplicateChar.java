6af43244f3d0a7d5886f0dff304442faf688b33b
Hussain <hussain.psum@gmail.com>
2023-11-16 22:06:44 +0530

    added one file

--- src/test/java/DSA/DuplicateChar.java
+++ src/test/java/DSA/DuplicateChar.java
@@ -1,18 +1,21 @@
 package DSA;
 
 import java.util.HashSet;
+import java.util.LinkedHashSet;
 
 public class DuplicateChar {
 
     public static void main(String[] args) {
         String str = "Hussain";
-        HashSet<Character> set = new HashSet<Character>();
+        LinkedHashSet<Character> set = new LinkedHashSet<Character>();
         StringBuilder f = new StringBuilder();
         for(char s : str.toCharArray()){
-            if(set.add(s)){
-                f.append(s);
-            }
+           set.add(s);
         }
+        for(char s : set){
+            f.append(s);
+        }
+        System.out.println(f);
     }
 
 
